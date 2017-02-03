f1601e1b37dbcf62a4a9f5bedbc550bd4192a152
Artem <Artem@DESKTOP-GJ9HCEP>
2017-02-03 18:55:49 +0000

    + Changed SFML link from dynamic to staic + Moved vertex and colour arrays from the stack to the heap to prevent stack overflow + Allowed for a significant increase in # of particles + Colour change based on speed of the particles + Minor fixes

--- SFML test/Utils.h
+++ SFML test/Utils.h
@@ -2,6 +2,9 @@
 
 #include <SFML/Graphics.hpp>
 
+#include <iostream>
+#include <Windows.h>
+
 using namespace std;
 
 inline float Dist(const sf::Vector2f &v, const sf::Vector2f &u)
@@ -12,4 +15,31 @@ inline float Dist(const sf::Vector2f &v, const sf::Vector2f &u)
 inline float Norm(const sf::Vector2f &v)
 {
 	return sqrt(pow(v.x, 2) + pow(v.y, 2));
+}
+
+inline float Clamp(const float f,const float min,const float max)
+{
+	if (f > max)
+		return max;
+	else if (f < min)
+		return min;
+	else
+		return f;
+}
+
+// FPS
+HANDLE _stdOutputH = GetStdHandle(STD_OUTPUT_HANDLE);
+CONSOLE_SCREEN_BUFFER_INFO _cTMPBuffer;
+
+CONSOLE_SCREEN_BUFFER_INFO _cFPSBuffer;
+
+inline void PrintAt(const string text, const CONSOLE_SCREEN_BUFFER_INFO &crsPos)
+{
+	GetConsoleScreenBufferInfo(_stdOutputH, &_cTMPBuffer); // remember the current position of the cursor
+	
+	SetConsoleCursorPosition(_stdOutputH, crsPos.dwCursorPosition); // move the cursor to the desired position
+	cout << text << endl;
+
+	SetConsoleCursorPosition(_stdOutputH, _cTMPBuffer.dwCursorPosition); // put the cursor back
+
 }
--- SFML test/main.cpp
+++ SFML test/main.cpp
@@ -6,12 +6,12 @@
 #include "Particle.h"
 #include "Utils.h"
 
-#define PARRAYHEIGHT 70 //width of the array of particles
-#define PARRAYWIDTH 70 //height of the array of particles
+#define PARRAYHEIGHT 1000 //width of the array of particles
+#define PARRAYWIDTH 1000 //height of the array of particles
 
 #define INITIAL_POS_X 0
 #define INITIAL_POS_Y 0
-#define SPREAD 5 //distance between 2 particles next to each other
+#define SPREAD 6 //distance between 2 particles next to each other
 
 using namespace std;
 
@@ -43,35 +43,41 @@ int main()
 	}
 
 	sf::Clock deltaTime; // to keep track of updates ( = dt)
-	float dt = 0.0000001; // init (dt = 1/framerat)
+	float dt = 0.0000001f; // init (dt = 1/framerat)
 
 	sf::Vector2f massiveObject; // will be able to place with LMB click
 
 	//OpenGL draw setup
-	float vertexCoords[2 * PARRAYHEIGHT * PARRAYWIDTH]; //a static coord array eg [1x, 1y, 2x, 2y, ..., nx, ny]
-	unsigned char colours[3 * PARRAYHEIGHT * PARRAYWIDTH]; //a static array for colours eg [1r,1g,1b,2r,2g,...,nr,ng,nb]
+	// Note we alocate both arrays on the heap to prevent stack overflow
+	float* vertexCoords = new float [2 * PARRAYHEIGHT * PARRAYWIDTH]; //a static coord array eg [1x, 1y, 2x, 2y, ..., nx, ny]
+	unsigned char* colours = new unsigned char [3 * PARRAYHEIGHT * PARRAYWIDTH]; //a static array for colours eg [1r,1g,1b,2r,2g,...,nr,ng,nb]
 
 	glEnable(GL_POINT_SMOOTH); // rounded dots
 	glEnable(GL_BLEND); glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA); //transparency blend
 	glPointSize(2); //size of a dot
 
+
+	cout << "FPS: "; GetConsoleScreenBufferInfo(_stdOutputH, &_cFPSBuffer); // setup to print fps
+
 	bool running = true; //continue to run the program
-	while (running)
+	while (running) // main loop
 	{
-		//handle events
+		//handle window events
 		sf::Event event;
 		while (window.pollEvent(event))
 		{
-			switch (event.type) 
+			switch (event.type)
 			{
-				case sf::Event::Closed:
-					running = false;
-					break;
-				case sf::Event::Resized:
-					glViewport(0, 0, event.size.width, event.size.height);
-					break;
+			case sf::Event::Closed:
+				running = false;
+				break;
+			case sf::Event::Resized:
+				glViewport(0, 0, window.getSize().x, window.getSize().y); // re-init viewport
+				glMatrixMode(GL_PROJECTION);
+				glLoadIdentity(); glOrtho(0, window.getSize().x, window.getSize().y, 0, -1, 1);
+				break;
 
-				default: ;
+			default:;
 			}
 		}
 
@@ -94,35 +100,27 @@ int main()
 		else if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
 			camPos.x -= 500 * dt / zoom;
 
-		
 
-		sf::Vector2f mousePos = sf::Vector2f(sf::Mouse::getPosition(window))/zoom - camPos
-			- (sf::Vector2f(window.getSize())/2.f)/zoom;
-		//cout << mousePos.x << " " << mousePos.y << endl;
 
-		if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) //add massiveObject on LMB click
-			massiveObject = mousePos;
+		sf::Vector2f mousePos = sf::Vector2f(sf::Mouse::getPosition(window)) / zoom - camPos
+			- (sf::Vector2f(window.getSize()) / 2.f) / zoom; // mpouse position in the game coordinates
 
-		float maxAcc = 0.f; // maximum acceleration/focre on a particle at a given time
+		if (sf::Mouse::isButtonPressed(sf::Mouse::Left)) //on LMB click
+			massiveObject = mousePos; // place massive object
 
 		for (unsigned i = 0; i < PARRAYHEIGHT * PARRAYWIDTH; ++i)
 		{
 			sf::Vector2f pPos = particles[i].GetPos();
-			particles[i].addForce( 10000.f * sf::Vector2f(mousePos - pPos) / float(pow( Dist(mousePos, pPos), 2))
-				+ 0.00001f * sf::Vector2f(mousePos - pPos) ); // gravity to mouse 
+
+			particles[i].addForce(10000.f * sf::Vector2f(mousePos - pPos) / pow(Dist(mousePos, pPos), 2)
+				+ 0.00001f * sf::Vector2f(mousePos - pPos)); // gravity to mouse 
 					//+ some constatnt force towards the mouse so we don't lose particles
 			if (massiveObject.x != 0.f && massiveObject.y != 0.f) // if massive obvect is placed
 				particles[i].addForce(sf::Vector2f(massiveObject - pPos) * 11000.f // gravity to the massive object
-					/ float(pow(Dist(massiveObject, pPos), 2)) );
-						
+					/ float(pow(Dist(massiveObject, pPos), 2)));
+
 
 			particles[i].updatePos(dt);
-
-
-			if (Norm(particles[i].getAcc()) > maxAcc) // find the maximum force/acceleration on a particle
-				maxAcc = Norm(particles[i].getAcc());
-
-
 
 			particles[i].clearForces(); // clear the forces after the position has been updated
 		}
@@ -131,7 +129,7 @@ int main()
 		for (unsigned i = 0; i < PARRAYHEIGHT * PARRAYWIDTH; ++i)
 		{
 			colours[3 * i] = 255;
-			colours[3 * i + 1] = 225 - int(150.f * Norm(particles[i].getAcc()) / maxAcc); // colour based on the relative acceleration
+			colours[3 * i + 1] = Clamp(225.f - particles[i].GetSpeed(), 0.f, 255.f); // colour based on speed
 			colours[3 * i + 2] = 255;
 
 			vertexCoords[2 * i] = particles[i].GetPos().x;
@@ -140,17 +138,19 @@ int main()
 
 		glPushMatrix(); //create a temp matrix to draw from
 
-		glTranslatef(window.getSize().x / 2.f, window.getSize().y / 2.f, 0); //center the camera to the middle on the window 
+		glTranslatef(window.getSize().x / 2.f, window.getSize().y / 2.f, 0); //make the center of the screen the origin 
+
 		glScaled(zoom, zoom, zoom); //apply zoom around the center of the window
 
 		glTranslatef(camPos.x, camPos.y, 0); // apply the camPos shift
 
 		glEnableClientState(GL_VERTEX_ARRAY); // state rendering method
-		glEnableClientState(GL_COLOR_ARRAY); 
+		glEnableClientState(GL_COLOR_ARRAY);
 
+		
 		glVertexPointer(2, GL_FLOAT, 0, vertexCoords); //buffer the coords
 		glColorPointer(3, GL_UNSIGNED_BYTE, 0, colours); //buffer the colours
-		glDrawArrays(GL_POINTS, 0, PARRAYHEIGHT * PARRAYWIDTH); //actual draw call
+		glDrawArrays(GL_POINTS, 0,PARRAYHEIGHT * PARRAYWIDTH); //actual draw call
 
 			//draw clean-up
 		glDisableClientState(GL_VERTEX_ARRAY);
@@ -163,7 +163,12 @@ int main()
 		window.display();
 
 		dt = deltaTime.restart().asSeconds(); //update dt
+		
+		PrintAt( to_string(1.f/dt), _cFPSBuffer); // print fps
 	}
+
+	delete[] vertexCoords; // clean up
+	delete[] colours;
 
 	return 0;
 }
